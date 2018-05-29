05e8bfd2eb8d0886cbe446dce6c3d7cca5a7c1dd
sergio <sheresergio@gmail.com>
2018-05-29 11:31:02 +0100

    Auto Log in: The app is able to remember if an user is already logged. If it is, it will go directly to the app menu, skipping the login menu.
    Log out button: implemented a button that let the user log out.

--- app/src/main/java/com/messageimposible/messageimpossible/ActivitySplashScreen.java
+++ app/src/main/java/com/messageimposible/messageimpossible/ActivitySplashScreen.java
@@ -7,35 +7,63 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class ActivitySplashScreen extends AppCompatActivity {
+
+    private ImageView iv;
+    private Animation an;
+
+    //Firebase
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_splash_screen);
 
-        final ImageView iv = findViewById(R.id.image_loading);
-        final Animation an = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+        iv = findViewById(R.id.image_loading);
+        an = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+        mAuth = FirebaseAuth.getInstance();
 
         iv.startAnimation(an);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-
-                Intent i = new Intent(ActivitySplashScreen.this, ActivityLogin.class);
-
-                startActivity(i);
-
-                finish();
-
-            }
-        }, 4000);
-
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser!=null){
+
+            Toast.makeText(this, "Usuario ya logeado.", Toast.LENGTH_LONG).show();
+
+            Intent i = new Intent(ActivitySplashScreen.this, ActivityTabs.class);
+            startActivity(i);
+            finish();
+
+        }else{
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    Intent i = new Intent(ActivitySplashScreen.this, ActivityLogin.class);
+
+                    startActivity(i);
+
+                    finish();
+
+                }
+            }, 4000);
+
+        }
+    }
+
 }
