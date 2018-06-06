060cb3388956b9a7e8e19a596932cadbeb3eb9f0
EstalinOrlando <estalineopg7gmail.com>
2018-06-06 11:38:15 +0100

    Merge branch 'master' of C:\Users\Estalin Orlando\Documents\Message with conflicts.

--- app/src/main/java/com/messageimposible/messageimpossible/Activity/ActivityTabs.java
+++ app/src/main/java/com/messageimposible/messageimpossible/Activity/ActivityTabs.java
@@ -2,11 +2,15 @@ package com.messageimposible.messageimpossible.Activity;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.TabLayout;
@@ -26,18 +30,28 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.messageimposible.messageimpossible.Entity.EntityUsers;
 import com.messageimposible.messageimpossible.Fragment.FragmentChats;
 import com.messageimposible.messageimpossible.Fragment.FragmentContacts;
 import com.messageimposible.messageimpossible.Fragment.FragmentInvites;
 import com.messageimposible.messageimpossible.R;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class ActivityTabs extends AppCompatActivity {
 
@@ -46,15 +60,22 @@ public class ActivityTabs extends AppCompatActivity {
 
     private Button deletePic;
     private Button selectPic;
-    private ImageView profilePic;
+    private ImageView profile_Pic;
+    //private final Integer PHOTO_PERFIL = 1;
+    private static final int PHOTO_PERFIL = 1;
+    //final int REQUEST_CODE_GALLERY = 999;
+    //final int REQUEST_CODE_CAMERA = 1313;
+    Bitmap bitmap;
+    Uri uri;
 
     private Dialog dialog;
 
 
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
+    private StorageReference storageReference;
+    private FirebaseStorage storage;
 
-    private String USER_ID;
     private String USER_NAME;
     private String USER_EMAIL;
 
@@ -72,6 +93,7 @@ public class ActivityTabs extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
@@ -83,10 +105,16 @@ public class ActivityTabs extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        deletePic = (Button) findViewById(R.id.deletePic);
-        selectPic = (Button) findViewById(R.id.selectPic);
-        profilePic = (ImageView) findViewById(R.id.profilePic);
+        deletePic = findViewById(R.id.deletePic);
+        selectPic = findViewById(R.id.selectPic);
+        defineView();
 
+
+
+    }
+
+    private void defineView(){
+        profile_Pic = findViewById(R.id.profilePic);
     }
 
 
@@ -155,13 +183,11 @@ public class ActivityTabs extends AppCompatActivity {
             Intent i = new Intent(this, ActivityAddFriend.class);
             i.putExtra("name", USER_NAME);
             i.putExtra("email", USER_EMAIL);
-            i.putExtra("id", USER_ID);
             startActivity(i);
 
         }else if(res_id == R.id.action_profile){
 
             //Toast.makeText(getApplicationContext(), "add friend", Toast.LENGTH_LONG).show();
-
             showDialog();
 
         }
@@ -180,7 +206,7 @@ public class ActivityTabs extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        final FirebaseUser currentUser = mAuth.getCurrentUser();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser!=null){
 
             DatabaseReference reference = database.getReference("users/"+currentUser.getUid());
@@ -190,7 +216,6 @@ public class ActivityTabs extends AppCompatActivity {
                 public void onDataChange(DataSnapshot dataSnapshot) {
 
                     EntityUsers user = dataSnapshot.getValue(EntityUsers.class);
-                    USER_ID = currentUser.getUid();
                     USER_NAME = user.getUsername();
                     USER_EMAIL = user.getEmail();
                     //Toast.makeText(ActivityTabs.this, USER_NAME , Toast.LENGTH_LONG).show();
@@ -221,6 +246,7 @@ public class ActivityTabs extends AppCompatActivity {
         Button selectPic = (Button) dialog.findViewById(R.id.selectPic);
 
 
+
         deletePic.setEnabled(true);
         selectPic.setEnabled(true);
 
@@ -235,15 +261,133 @@ public class ActivityTabs extends AppCompatActivity {
         selectPic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "select", Toast.LENGTH_LONG).show();
 
+                Toast.makeText(getApplicationContext(), "select", Toast.LENGTH_LONG).show();
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.setType("image/jpeg");
+                i.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
+                startActivityForResult(Intent.createChooser(i,"Select Profile Picture"),PHOTO_PERFIL);
+
+
+                //openGallery();
             }
         });
-
 
         dialog.show();
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PHOTO_PERFIL && resultCode == RESULT_OK) {
+            Uri u = data.getData();
+            storageReference = storage.getReference("profilePic");//imagenes_chat
+            final StorageReference fotoReferencia = storageReference.child(u.getLastPathSegment());
+            fotoReferencia.putFile(u).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    //Glide.with(ActivityTabs.this).load(u.toString()).into(profile_Pic);
+                }
+            });
+        }
+
+    }
+
+    /*
+    public void openGallery(){
+        //Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        //startActivityForResult(gallery, PHOTO_PERFIL);
+
+            Intent intent = new Intent();
+            // Show only images, no videos or anything else
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            // Always show the chooser (if there are multiple options available)
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PHOTO_PERFIL);
+
+    }
+    */
+/*
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PHOTO_PERFIL && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            uri = data.getData();
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+                Toast.makeText(this, "hey you selected image" + bitmap, Toast.LENGTH_SHORT).show();
+                //profile_Pic.setImageBitmap(bitmap);
+                ImageView profile_Pic =  (ImageView) dialog.findViewById(R.id.profilePic);
+                profile_Pic.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    */
+
+    /*
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PHOTO_PERFIL && resultCode == RESULT_OK){
+            Uri u = data.getData();
+            profilePic = findViewById(R.id.profilePic);
+            profilePic.setImageURI(u);
+        }
+    }
+    */
+
+
+/*
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == REQUEST_CODE_GALLERY){
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_GALLERY);
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "You don't have access to Galery", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK && data != null){
+            Uri uri = data.getData();
+
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(uri);
+
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                profilePic.setImageBitmap(bitmap);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }else if(requestCode==REQUEST_CODE_CAMERA && resultCode == RESULT_OK && data != null){
+
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            profilePic.setImageBitmap(bitmap);
+
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    */
 }
 
--- app/src/main/java/com/messageimposible/messageimpossible/Adapter/AdapterMessage.java
+++ app/src/main/java/com/messageimposible/messageimpossible/Adapter/AdapterMessage.java
@@ -6,27 +6,20 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import com.messageimposible.messageimpossible.Entity.EntityMessage;
 import com.messageimposible.messageimpossible.Entity.EntityMessageTarget;
 import com.messageimposible.messageimpossible.Holder.HolderMessage;
-import com.messageimposible.messageimpossible.Holder.HolderMessageTarget;
 import com.messageimposible.messageimpossible.R;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-
-public class AdapterMessage extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class AdapterMessage extends RecyclerView.Adapter<HolderMessage> {
 
     private List<EntityMessageTarget> messageList = new ArrayList<>();
     private Context c;
-    private String currentUserID;
-
-    private static final int MESSAGE_OWNER = 0;
-    private static final int MESSAGE_TARGET = 1;
-
 
     public AdapterMessage(Context c) {
         this.c = c;
@@ -39,64 +32,32 @@ public class AdapterMessage extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
     }
 
-    public void setCurrentUserID(String id){
-
-        this.currentUserID = id;
-
-    }
-
-
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public HolderMessage onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View v = LayoutInflater.from(c).inflate(R.layout.item_listview_inchat_owner, parent,false);
 
-        if (viewType == MESSAGE_OWNER) {
-            View v = layoutInflater.inflate(R.layout.item_listview_inchat_owner, parent, false);
-
-            return new HolderMessage(v);
-        } else {
-            View v = layoutInflater.inflate(R.layout.item_listview_inchat_target, parent, false);
-
-            return new HolderMessageTarget(v);
-        }
-
+        return new HolderMessage(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull HolderMessage holder, int position) {
 
-        EntityMessageTarget item = messageList.get(position);
+        holder.getMessage().setText(messageList.get(position).getMessage());
 
-        if(holder instanceof HolderMessage){
+        Long timeCode = messageList.get(position).getTime();
+        Date d = new Date(timeCode);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
 
-            ((HolderMessage) holder).bind(item);
-
-        }else{
-
-            ((HolderMessageTarget) holder).bind(item);
-
-        }
-
+        holder.getTime().setText(sdf.format(d));
 
     }
-
 
     @Override
     public int getItemCount() {
         return messageList.size();
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        if (messageList.get(position).getId().equals(currentUserID)) {
-            return MESSAGE_OWNER;
-        } else {
-            return MESSAGE_TARGET;
-        }
-
     }
 
 }
